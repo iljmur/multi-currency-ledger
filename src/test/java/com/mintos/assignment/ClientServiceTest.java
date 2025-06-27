5931df32e4d0fdfd756ff714f50bd1c722f21424
Ilja Murasko <iljamurasko@cloud.upwork.com>
2025-06-27 14:42:01 +0300

    empty test report for start

---
+++ src/test/java/com/mintos/assignment/ClientServiceTest.java
@@ -0,0 +1,12 @@
+package com.mintos.assignment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ClientServiceTest {
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+}
